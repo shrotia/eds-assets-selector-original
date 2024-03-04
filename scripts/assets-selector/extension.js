abaa013701589be3ec22c9f696291a07478fef17
shrotia <shroti@adobe.com>
2024-03-04 21:55:46 +0530

    Update extension.js

--- scripts/assets-selector/extension.js
+++ scripts/assets-selector/extension.js
@@ -1,13 +1,22 @@
-const customBlocksNameConfig = new Map([['video', 'core embed']]);
-
-customPrintSuccess = () => {
-    console.log("Copied to Clipboard - custom cod ssse");
-}
-
+/**
+ * Creates a video block and copies it to the clipboard.
+ *
+ * @param {Object} asset - The asset object.
+ * @param {Object} rendition - The rendition object.
+ * @returns {undefined} - The function does not return anything.
+ */
 function modifiedCreateVideoBlock (asset, rendition) {
    return copyToClipboardWithBlock(asset.name, rendition.href, 'Core Embed (V2)');
 }
 
+/**
+ * Copies a block of HTML to the clipboard.
+ *
+ * @param {string} name - The name to be displayed in the block.
+ * @param {string} link - The link to be embedded in the block.
+ * @param {string} blockName - The name of the block.
+ * @returns {Promise<Object>} - A promise that resolves to an object with a success property indicating whether the operation was successful, and an error property that will be null if the operation was successful.
+ */
 async function copyToClipboardWithBlock(name, link, blockName) {
     const block = `
     <table border='1'>
